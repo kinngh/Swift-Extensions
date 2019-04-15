7d6060e7c326d60c8ed3af3b6cb1106668113570
Harshdeep Singh Hura <kinngh@me.com>
2019-04-15 21:42:24 +0530

    Update and rename playSound.swift to playSystemSound.swift

--- functions/audio/playSystemSound.swift
+++ functions/audio/playSystemSound.swift
@@ -1,6 +1,15 @@
 import AudioToolBox
 
-func playAudio(nameAudio: String, formatAudio: String){
+
+/*
+
+- No longer than 30 seconds in duration
+- In linear PCM or IMA4 (IMA/ADPCM) format
+- Packaged in a .caf, .aif, or .wav file
+
+*/
+
+func playSystemSound(nameAudio: String, formatAudio: String){
 
     if let soundURL = Bundle.main.url(forResource: nameAudio, withExtension: formatAudio){
         var mySound: SystemSoundID = 0
